7e1bdb653a483d8f053dbcc2a600b22ea8680591
mayuping <myp@qq.com>
2018-12-19 15:59:40 +0800

    登录页完成

---
+++ src/components/Login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="login">
+    <el-form ref="form" :model="form" label-width="80px">
+  <el-form-item label="用户名">
+    <el-input v-model="form.usename"></el-input>
+  </el-form-item>
+  <el-form-item label="密码">
+    <el-input v-model="form.password"></el-input>
+  </el-form-item>
+  <el-form-item>
+    <el-button type="primary" @click="onSubmit">立即创建</el-button>
+    <el-button>取消</el-button>
+  </el-form-item>
+</el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      form: {
+        usename: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.login{
+  width: 100%;
+  height: 100%;
+  background-color: #2d434c;
+  overflow: hidden;
+  .el-form{
+    width: 400px;
+    background-color: #fff;
+    margin:200px auto;
+    padding: 75px 40px 15px;
+  }
+}
+</style>
